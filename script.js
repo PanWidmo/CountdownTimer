5830676252b58be37586309e8e65a0c1259a8103
PanWidmo <tomasz.baranww@gmail.com>
2021-10-11 16:12:13 +0200

    Style improvements and added protection to prevent negative values

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const hoursDisplay = document.getElementById('hours');
 const minDisplay = document.getElementById('min');
 const secDisplay = document.getElementById('sec');
 
+const button = document.getElementById('btn');
+const container = document.getElementById('countdown');
 
 function countdown() {
     const newYears = document.getElementById('dateSelected').value;
@@ -12,17 +14,26 @@ function countdown() {
 
     const totalSeconds = (newYearsDate - currentDate) / 1000;
 
-    const days = Math.floor(totalSeconds / 3600 / 24);
-    const hours = Math.floor(totalSeconds / 3600) % 24;
-    const minutes = Math.floor(totalSeconds / 60) % 60;
-    const seconds = Math.floor(totalSeconds) % 60;
+    if(totalSeconds < 0){
+        alert ('Wrong date selected');
+    }
 
-    daysDisplay.innerHTML = days;
-    hoursDisplay.innerHTML = hours;
-    minDisplay.innerHTML = formatTime(minutes);
-    secDisplay.innerHTML = formatTime(seconds);
+    else {
+        const days = Math.floor(totalSeconds / 3600 / 24);
+        const hours = Math.floor(totalSeconds / 3600) % 24;
+        const minutes = Math.floor(totalSeconds / 60) % 60;
+        const seconds = Math.floor(totalSeconds) % 60;
 
-    setInterval(countdown, 1000);
+        daysDisplay.innerHTML = days;
+        hoursDisplay.innerHTML = hours;
+        minDisplay.innerHTML = formatTime(minutes);
+        secDisplay.innerHTML = formatTime(seconds);
+
+        button.style.visibility = 'hidden';
+        container.style.visibility = 'visible';
+
+        setInterval(countdown, 1000);
+    }
 }
 
 function formatTime(time) {
